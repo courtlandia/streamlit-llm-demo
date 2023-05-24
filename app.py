feabb3e654bfcb57ef8f7b88a774a958fa1db750
courtlandia <99390213+courtlandia@users.noreply.github.com>
2023-05-24 15:36:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import openai
 
 # Set page configuration
 st.set_page_config(layout="wide")
 
-# Create a sidebar with file upload functionality
+# Create a sidebar with file upload functionality and OpenAI API key input
 st.sidebar.title("Upload CSV")
 uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 
+st.sidebar.title("OpenAI Settings")
+openai_api_key = st.sidebar.text_input("Enter OpenAI API Key", type="password")
+
+# Configure OpenAI API
+openai.api_key = openai_api_key
+
 # Main page content
-st.title("Data Visualization with Altair")
-st.write("Upload a CSV file and select columns to visualize.")
+st.title("Data Visualization and Question Answering with Altair and OpenAI")
+st.write("Upload a CSV file and select columns to visualize. Ask questions about the data using OpenAI.")
 
 # Load data if a file has been uploaded
 if uploaded_file is not None:
@@ -38,3 +45,24 @@ if uploaded_file is not None:
     # Display the chart
     st.subheader("Data Visualization")
     st.altair_chart(chart, use_container_width=True)
+
+    # Allow the user to ask questions using OpenAI
+    question = st.text_input("Ask a question about the data")
+
+    if st.button("Ask"):
+        if openai_api_key:
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=question,
+                max_tokens=100,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+
+            answer = response.choices[0].text.strip()
+            st.subheader("Answer")
+            st.write(answer)
+        else:
+            st.warning("Please enter your OpenAI API key in the sidebar.")
+
